c8471c5faacf664c1b026357a5ec6d8d31b24489
Henri-M-8 <henri.a.m8@gmail.com>
2026-03-29 09:51:04 -0400

    Create recipes.py

---
+++ Assignment5/api/controllers/recipes.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from ..models import models
+from fastapi import Response, status
+
+def create(db: Session, recipe):
+    db_recipe = models.Recipe(
+        sandwich_id=recipe.sandwich_id,
+        resource_id=recipe.resource_id,
+        amount=recipe.amount
+    )
+    db.add(db_recipe)
+    db.commit()
+    db.refresh(db_recipe)
+    return db_recipe
+
+def read_all(db: Session):
+    return db.query(models.Recipe).all()
+
+def read_one(db: Session, recipe_id):
+    return db.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
+
+def update(db: Session, recipe_id, recipe):
+    query = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    update_data = recipe.dict(exclude_unset=True)
+    query.update(update_data, synchronize_session=False)
+    db.commit()
+    return query.first()
+
+def delete(db: Session, recipe_id):
+    query = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
